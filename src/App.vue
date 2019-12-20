accaf863da3e3df0cce9076162b1c211005c4005
jeremi gendron <gendronjeremi@gmail.com>
2019-12-20 02:14:41 -0500

    init: Vuetify and color design; logo design, slider logic
    Next step: Setup the personality of the website.
    Then fill the slides and add proper transitions.
    After just get creative lmao

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,174 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2 mb-2"
+          contain
+          :src="logo"
+          transition="scale-transition"
+          width="125"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        rounded
+        class="navbar_button"
+      >
+        Services
+      </v-btn>
+      <v-btn
+        text
+        rounded
+        class="navbar_button"
+      >
+        About us
+      </v-btn>
+      <v-btn
+        text
+        rounded
+        class="navbar_button"
+      >
+        Contact
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-divider
+        vertical
+      ></v-divider>
+      <div
+        class="ml-3"
+      >
+        <v-btn
+          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          target="_blank"
+          text
+          rounded
+          outlined
+          elevation="2"
+          class="navbar_button mr-2"
+        >
+          <v-icon size="18">mdi-email</v-icon>
+        </v-btn>
+        <v-btn
+          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          target="_blank"
+          text
+          rounded
+          outlined
+          elevation="2"
+          class="navbar_button"
+        >
+          <v-icon size="18">mdi-phone</v-icon>
+        </v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-content>
+      <v-carousel
+        height="100%"
+        active-class="active_slide"
+        hide-delimiter-background
+        :vertical-delimiters="true"
+        :continuous="true"
+        :cycle="true"
+        :interval="10000"
+      >
+        <v-carousel-item
+          :style="{
+            backgroundImage: `url('${carpet_living_room}')`,
+            backgroundSize: 'cover',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }"
+        >
+          <div
+            :style="{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100%'
+            }"
+          >
+            <span>test</span>
+          </div>
+        </v-carousel-item>
+        <v-carousel-item
+          :style="{
+            backgroundImage: `url('${carpet_living_room}')`,
+            backgroundSize: 'cover',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }"
+        >
+          <div
+            :style="{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100%'
+            }"
+          >
+            <span>test</span>
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    logo: require('./assets/logo_horiz_turq_whitetext.png'),
+    carpet_living_room: require('./assets/slides/carpet_living_room.jpg'),
+    phone_number: '450-218-1279'
+  }),
+
+  computed: {
+
+  }
+};
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: "Babylove";
+  src: url("assets/fonts/Babylove-zGwX.ttf") format("woff2");
 }
 
-#nav {
-  padding: 30px;
+.navbar_button {
+  font-family: 'Babylove' !important
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.active_slide {
+  background-color: red
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+</style>
+<style lang="scss">
+.v-window { 
+  &-x-transition, 
+  &-x-reverse-transition, 
+  &-y-transition, 
+  &-y-reverse-transition { 
+    &-enter-active, 
+    &-leave-active { 
+      transition: .6s cubic-bezier(.70, .8, .50, 1) !important; 
+    } 
+  } 
 }
 </style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      light: {
+        primary: '#009ae4',
+        secondary: '#2196f3',
+        accent: '#4caf50',
+        error: '#f44336',
+        info: '#cddc39',
+        success: '#8bc34a',
+        warning: '#ffeb3b'
+      },
+    },
+  },
+});
