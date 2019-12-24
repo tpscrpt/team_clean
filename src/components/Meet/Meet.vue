5af114853d81f2173bc18de148aac7b412ffab72
jeremi gendron <gendronjeremi@gmail.com>
2019-12-23 19:26:30 -0500

    meet: refine spacing and styling of team member section

--- src/components/Meet/Meet.vue
+++ src/components/Meet/Meet.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="meet_container flex-column align-center justify-center">
+  <div class="meet_container flex-column align-center justify-center"
+    :style="{
+      padding: sn(0, 2, 3, 4, 5, 'vw'),
+    }"
+  >
     <div class="meet_title_container">
-      <h2 class="meet_title"></h2>
+      <h2 class="meet_title"
+        :style="{
+          fontSize: sn(38, 32, 40, 48, 56),
+          textDecoration: 'underline'
+        }"          
+
+      >Meet Team Clean</h2>
     </div>
 
     <div class="meet_members_container d-flex flex-wrap align-start justify-center"
       :style="{
-        width: '100%'
+        width: '100%',
       }"
     >
       <Member v-for="(member, index) in members" :key="index" :member="member"/>
@@ -36,5 +46,10 @@ export default {
 <style>
 .meet_container {
   width: 100%;
+}
+.meet_title {
+  text-align: center;
+  font-family: 'Dancing Script', cursive;
+  color: var(--v-secondary-darken4)
 }
 </style>
--- src/components/Meet/Member.vue
+++ src/components/Meet/Member.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="member_container flex-column align-center justify-start"
     :style="{
-      width: sn(90, 40, 20, 20, 20, 'vw'),
+      width: sn(90, 70, 35, 20, 20, 'vw'),
       padding: s(12, 4),
+      margin: sn(1, 2, 2, 2, 3, 'vw'),
       marginBottom: s(10, 0),
     }"
   >
@@ -12,16 +13,26 @@
     >
       <img class="member_img" :src="member.img"
         :style="{
-          maxWidth: '100%'
+          width: sn(150, 150, 200, 200, 220)
         }"
       />
     </div>
     <br>
     <div class="member_name_container text-center">
-      <h3 class="member_name">{{member.name}}</h3>
+      <h3 class="member_name"
+        :style="{
+          fontSize: sn(22, 22, 24, 24, 24)
+        }"
+      >
+        {{member.name}}
+      </h3>
     </div>
-    <div class="member_title_container text-center">
-      <h3 class="member_title">{{member.title}}</h3>
+    <div class="member_title_container text-left">
+      <h3 class="member_title"
+        :style="{
+          fontSize: sn(18, 18, 20, 22, 22)
+        }"
+      >{{member.title}}</h3>
     </div>
     <br>
     <div class="member_excerpt_container">
@@ -46,13 +57,17 @@ export default {
 <style>
 .member_name {
   font-family: 'Babylove';
-  letter-spacing: 2px
+  letter-spacing: 2px;
+  color: var(--v-accent-darken1);
 }
 .member_title {
   font-family: 'Poppins', sans-serif;
+  color: var(--v-primary-darken3)
 }
 .member_excerpt {
-  text-align: justify
+  text-align: justify;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
 }
 .member_img {
   border-radius: 50%
